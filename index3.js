80be43b9195d4d6610e620dc259449c6ab290fba
stephanie <johanna.garatecar@gmail.com>
2017-06-26 19:16:39 -0700

    arraycuatro

--- index3.js
+++ index3.js
@@ -1,17 +1,13 @@
 var assert = require("assert");
 
-var ejercicio3=function(aleatorios,x){
-  var respuesta = [] ;
-  aleatorios.forEach(n => {
-    if (n%x==0)
-    respuesta.push(n);
-  });
-  return respuesta;
+var ejercicio4=function(arreglo){
+
+  return arreglo.map( n=> n*2 +1);
 }
 
 
-  describe("Ejercicio3", function(){
-    it("Prueba de [7,21,44,80,77,35] y 7 " , function(){
-      assert.equal(true , ejercicio2 ([121312341,435353535,435435435]));
+  describe("Ejercicio4", function(){
+    it("Prueba de [1,5,6,3,7,3,5,0] " , function(){
+      assert.equal([3,11,13,7,15,7,11,1] , ejercicio4 ([1,5,6,3,7,3,5,0]));
     });
   });
---
+++ index4.js
@@ -0,0 +1,13 @@
+var assert = require("assert");
+
+var ejercicio4=function(arreglo){
+
+  return arreglo.map( n=> n*2 +1);
+}
+
+
+  describe("Ejercicio4", function(){
+    it("Prueba de [1,5,6,3,7,3,5,0] " , function(){
+      assert.equal([3,11,13,7,15,7,11,1], ejercicio4 ([1,5,6,3,7,3,5,0]);
+    });
+  });
