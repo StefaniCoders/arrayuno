d234e9e42437efb51069dd4655ec630b3d8d0b2f
stephanie <johanna.garatecar@gmail.com>
2017-06-26 18:33:14 -0700

    arraysdos

---
+++ index2.js
@@ -0,0 +1,9 @@
+var celus =[121312341,435353535,435435435];
+var res= celus.filter(function(elemento){
+  elem=elem.toString();
+  num=num.toSString();
+  if(elem.substr(0,3)== num elem.substr(-3)==num){
+    return true;
+  else
+    return false;
+  });
