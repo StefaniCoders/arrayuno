39d0335d8ce0839043d26721ac343c040fda0394
stephanie <johanna.garatecar@gmail.com>
2017-06-26 19:03:00 -0700

    arraytres

--- index3.js
+++ index3.js
@@ -1,11 +1,17 @@
-var array = [7,21,44,80,77,35];
-var num=7;
-var resultado="";
+var assert = require("assert");
+
+var ejercicio3=function(aleatorios,x){
+  var respuesta = [] ;
+  aleatorios.forEach(n => {
+    if (n%x==0)
+    respuesta.push(n);
+  });
+  return respuesta;
+}
 
 
-array.forEach(function multiplo(valor){
-  if (valor&num==0){
-    resultado += valor+" ";
-  }
-});
-document.write(resultado);
+  describe("Ejercicio3", function(){
+    it("Prueba de [7,21,44,80,77,35] y 7 " , function(){
+      assert.equal(true , ejercicio2 ([121312341,435353535,435435435]));
+    });
+  });
